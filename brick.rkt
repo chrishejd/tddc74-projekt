aea8409390eb6ff5c67e4528b1360291d21cf7aa
jongu926 <jonatan_gsson@hotmail.com>
2015-04-15 16:43:38 +0200

    Started brick class
    Added 5 bricks, the bricks gets to y=500 after the canvas has been drawn. Solved now by the top-brick-coord.

--- brick.rkt
+++ brick.rkt
@@ -1,5 +1,7 @@
 #lang racket/gui
 (provide brick%)
+(provide brick-lst)
+(provide top-brick-coord)
 (define brick%
   (class object%
       (init-field x-pos y-pos width height color)
@@ -10,6 +12,11 @@
       (define/public (set-y-pos! new-pos)
         (set! y-pos new-pos))
         
+      (define/public (offset-x-y-pos x-offset y-offset)
+        (begin
+          (set! x-pos (+ x-pos x-offset))
+          (set! y-pos (+ y-pos y-offset))))
+        
       (define/public (set-color! new-color)
         (set! color new-color))
         
@@ -17,4 +24,65 @@
         (begin
           (set! width (+ width offset))
           (set! height (+ height offset))))
+          
+      (define/public (get-x-pos)
+        x-pos)
+        
+      (define/public (get-y-pos)
+      y-pos)
+      
+      (define/public (get-height)
+        height)
+      
+      (define/public (get-width)
+       width)
+       
+      (define/public (get-color)
+      color)
+       
   (super-new)))
+  
+  (define top-brick-coord -400)
+  
+  (define brick1
+	(new brick%
+		[x-pos 0]
+		[y-pos top-brick-coord]
+		[height 100]
+		[width 100]
+		[color "black"]))
+		
+(define brick2
+	(new brick%
+		[x-pos 100]
+		[y-pos (+ 100 top-brick-coord)]
+		[height 100]
+		[width 100]
+		[color "black"]))
+		
+(define brick3
+	(new brick%
+		[x-pos 200]
+		[y-pos (+ 200 top-brick-coord)]
+		[height 100]
+		[width 100]
+		[color "black"]))
+		
+(define brick4
+	(new brick%
+		[x-pos 300]
+		[y-pos (+ 400 top-brick-coord)]
+		[height 100]
+		[width 100]
+		[color "black"]))
+		
+(define brick5
+	(new brick%
+		[x-pos 400]
+		[y-pos (+ 300 top-brick-coord)]
+		[height 100]
+		[width 100]
+		[color "black"]))
+		
+(define brick-lst
+	(list brick1 brick2 brick3 brick4 brick5))
