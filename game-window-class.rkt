c880a6499dbd0cd9d3b641141a2558b26710f39e
chrishejd <chrillewushu@gmail.com>
2015-04-20 11:18:10 +0200

    Update game-window-class.rkt

--- game-window-class.rkt
+++ game-window-class.rkt
@@ -1,27 +1,10 @@
 #lang racket/gui
+(provide game-window)
 
-(define game-window
-  (class object%
-    [init-field (grid [make-bitmap 100 100]
-                      [x 0]
-                      [y 0])]
-                      
-    (define/public (render-game canvas dc)
-      (define (change-brick-pos lst)
-        (if (null? (cdr lst))
-          (begin
-            (send (car lst) offset-x-y-pos 0 100)
-            (send dc set-brush (make-object brush% (send (car lst) get-color) 'solid))
-            (send dc draw-rectangle (send (car lst) get-x-pos) (send (car lst) get-y-pos)
-                 (send (car lst) get-width) (send (car lst) get-height)))
-          (begin
-            (send (car lst) offset-x-y-pos 0 100)
-            (send dc set-brush (make-object brush% (send (car lst) get-color) 'solid))
-            (send dc draw-rectangle (send (car lst) get-x-pos) (send (car lst) get-y-pos)
-                  (send (car lst) get-width) (send (car lst) get-height))
-            (change-brick-pos (cdr lst)))))
-      (begin
-        (send dc set-background "red")
-        (send dc draw-bitmap grid-bitmap 0 0)
-        (change-brick-pos brick-lst))))
+(define game-canvas%
+  (class canvas%
+    (init-field [keyboard-handler display])
+    (define/override (on-char key-event)
+      (keyboard-handler key-event)))
     (super-new))
+    
