689f469cc77e17e84522edc23870d12e3193d4fe
chrishejd <chrillewushu@gmail.com>
2015-04-23 09:47:53 +0200

    23/4
    Moved the game window here.

--- game-window-class.rkt
+++ game-window-class.rkt
@@ -1,11 +1,22 @@
 #lang racket/gui
-(provide game-window)
+(provide game-canvas%)
+(provide main-game-window)
 
 (define game-canvas%
   (class canvas%
     (init-field [keyboard-handler display])
-    (define/override (on-char key-event)
-      (keyboard-handler key-event)))
-    (super-new))
     
+    (define/override (on-char key-event)
+      (keyboard-handler key-event))
+    
+    (super-new)))
+
+;;Parent window for game-mode
+(define main-game-window
+  (new frame%
+       [width 800]
+       [height 799]
+       [label "Inte nudda lava"]))
+
+
     
