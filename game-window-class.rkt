c871a406f445ee4595c0ac7f40ede32184f56f62
chrishejd <chrillewushu@gmail.com>
2015-05-04 12:22:23 +0200

    Update game-window-class.rkt

--- game-window-class.rkt
+++ game-window-class.rkt
@@ -1,6 +1,7 @@
 #lang racket/gui
 (provide game-canvas%)
 (provide main-game-window)
+(provide wrong-key-window)
 
 (define game-canvas%
   (class canvas%
@@ -11,12 +12,33 @@
     
     (super-new)))
 
+(define game-frame%
+  (class frame%
+    (init-field curr-score)
+    
+    (define/public (get-score)
+      curr-score)
+    
+    (define/public (inc-score)
+      (set! curr-score (+ 1 curr-score)))
+    
+    (define/public (init-score)
+      (set! curr-score 0))
+    (super-new)))
+
 ;;Parent window for game-mode
 (define main-game-window
-  (new frame%
+  (new game-frame%
        [width 800]
        [height 799]
-       [label "Inte nudda lava"]))
+       [label "Don't touch the lava!"]
+       [curr-score 0]))
+
+(define wrong-key-window
+  (new frame%
+       [width 500]
+       [height 299]
+       [label "Don't touch the lava!"]))
 
 
     
