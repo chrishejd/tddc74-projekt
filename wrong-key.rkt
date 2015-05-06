9be3f04f438bf47e15f4365a41f91d3836bc516f
chrishejd <chrillewushu@gmail.com>
2015-05-06 16:45:47 +0200

    Update wrong-key.rkt

--- wrong-key.rkt
+++ wrong-key.rkt
@@ -1,5 +1,7 @@
 #lang racket/gui
 (require "game-window-class.rkt")
+(require "highscore.rkt")
+(require "timer-start-window.rkt")
 (provide main-wrong-key)
 
 (define (render-wrong-key canvas dc)
@@ -18,22 +20,26 @@
        [parent wrong-key-window]
        [paint-callback render-wrong-key]))
 
+;;Main function for pressing the wrong key
 (define (main-wrong-key)
+  (send timer stop)
   (send main-game-window show #f)
+  (send classic-highscore update-score! (send main-game-window get-score))
   (send wrong-key-window show #t)
   (send wrong-key-window refresh))
 
 (define retry-button
   (new button%
        [parent wrong-key-window]
-       [label "Retry"]
+       [label "Play again"]
        [callback (lambda (button event) 
                    (send wrong-key-window show #f)
                    (send main-game-window show #t)
                    (send main-game-window refresh)
-                   (send main-game-window init-score))]))
+                   (send main-game-window init-score)
+                   (send start-window show #t))]))
 
 (define exit-button
   (new button%
        [parent wrong-key-window]
-       [label "Exit"]))
+       [label "Main menu"]))
