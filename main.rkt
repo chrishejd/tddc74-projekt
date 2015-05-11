1f7961608aeb30d324d3364728e19604a802316e
chrishejd <chrillewushu@gmail.com>
2015-05-11 12:42:52 +0200

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -4,8 +4,8 @@
 (require "game-window-class.rkt")
 (require "timer-start-window.rkt")
 (require "highscore.rkt")
+(require "main-menu.rkt")
 
 (init-bricks)
 (send classic-highscore load-highscore)
-(send main-game-window show #t)
-(send start-window show #t)
+(send menu-window show #t)
