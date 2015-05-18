10c93bc134452c244054ba511282975e4146dbe8
chrishejd <chrillewushu@gmail.com>
2015-05-18 11:42:46 +0200

    Rewrote the grid code
    Now the grid is drawn width an iterative function, which draws the grid in the right size. I still use choose-bitmap as a "main" function.

--- game-grid.rkt
+++ game-grid.rkt
@@ -1,74 +1,47 @@
 #lang racket/gui
 (require "highscore.rkt")
-(provide mk-grid-bitmap)
 (provide grid)
 (provide grid-bitmap)
 (provide choose-bitmap)
+(provide grid-edge)
 
-(define grid-bitmap (void))
+(define grid-edge (void))
 (define grid (void))
+(define grid-bitmap (make-bitmap 800 1600))
 
-(define grid-bitmap-5 (make-bitmap 800 1600))
+;;Draws a grid of width size
+(define (mk-grid size)
+  (let ((dc (new bitmap-dc% [bitmap grid-bitmap]))
+        (vert-size (* size 100)))
+    ;;Draws the vertical lines
+    (define (vert-lines-text col)
+      (if (= 100 col)
+          (begin
+            (send dc set-font (make-font #:size 20))
+            (send dc draw-text (number->string (/ col 100)) (- col 60) 725)
+            (send dc draw-line col 0 col 750))
+          (begin
+            (send dc draw-line col 0 col 750)
+            (vert-lines-text (- col 100))
+            (send dc set-font (make-font #:size 20))
+            (send dc draw-text (number->string (/ col 100)) (- col 60) 725))))
 
-(define (mk-grid-bitmap target)
-  (let ((dc (new bitmap-dc% [bitmap target])))
+    
     (begin
       (send dc set-brush (make-object brush% "red" 'solid))
-      (send dc draw-rectangle 0 0 500 700)
-      (send dc draw-line 100 0 100 750)
-      (send dc draw-line 200 0 200 750)
-      (send dc draw-line 300 0 300 750)
-      (send dc draw-line 400 0 400 750)
-      (send dc draw-line 500 0 500 750)
+      (send dc draw-rectangle 0 0 vert-size 700)
       ;;Horizontal lines
       (send dc draw-line 0 0 500 0)
-      (send dc draw-line 0 100 500 100)
-      (send dc draw-line 0 200 500 200)
-      (send dc draw-line 0 300 500 300)
-      (send dc draw-line 0 400 500 400)
-      (send dc draw-line 0 500 500 500)
-      (send dc draw-line 0 600 500 600)
-      (send dc draw-line 0 700 500 700)
-      (send dc set-font (make-font #:size 20))
-      (send dc draw-text "1" 40 725)
-      (send dc draw-text "2" 140 725)
-      (send dc draw-text "3" 240 725)
-      (send dc draw-text "4" 340 725)
-      (send dc draw-text "5" 440 725))))
+      (send dc draw-line 0 100 vert-size 100)
+      (send dc draw-line 0 200 vert-size 200)
+      (send dc draw-line 0 300 vert-size 300)
+      (send dc draw-line 0 400 vert-size 400)
+      (send dc draw-line 0 500 vert-size 500)
+      (send dc draw-line 0 600 vert-size 600)
+      (send dc draw-line 0 700 vert-size 700)
+      ;;Vertical lines
+      (vert-lines-text vert-size))))
 
-(define grid-5 (mk-grid-bitmap grid-bitmap-5))
-
-(define grid-bitmap-3 (make-bitmap 800 1600))
-
-(define (mk-grid-bitmap-3 target)
-  (let ((dc (new bitmap-dc% [bitmap target])))
-    (begin
-      (send dc set-brush (make-object brush% "red" 'solid))
-      (send dc draw-rectangle 0 0 300 700)
-      (send dc draw-line 100 0 100 750)
-      (send dc draw-line 200 0 200 750)
-      (send dc draw-line 300 0 300 750)
-      ;;Horizontal lines
-      (send dc draw-line 0 0 500 0)
-      (send dc draw-line 0 100 300 100)
-      (send dc draw-line 0 200 300 200)
-      (send dc draw-line 0 300 300 300)
-      (send dc draw-line 0 400 300 400)
-      (send dc draw-line 0 500 300 500)
-      (send dc draw-line 0 600 300 600)
-      (send dc draw-line 0 700 300 700)
-      (send dc set-font (make-font #:size 20))
-      (send dc draw-text "1" 40 725)
-      (send dc draw-text "2" 140 725)
-      (send dc draw-text "3" 240 725))))
-
-(define grid-3 (mk-grid-bitmap-3 grid-bitmap-3))
-
-(define (choose-bitmap columns)
-  (cond
-    ((= 5 columns) 
-     (set! grid-bitmap grid-bitmap-5)
-     (set! grid grid-5))
-    ((= 3 columns)
-     (set! grid-bitmap grid-bitmap-3)
-     (set! grid grid-3))))
+(define (choose-bitmap col)
+  (set! grid (mk-grid col))
+  (set! grid-edge (* 100 col)))
