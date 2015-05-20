629d7f6308f0cc81519b706cd1a3456bff1c496b
jongu926 <jonatan_gsson@hotmail.com>
2015-05-20 16:47:52 +0200

    Update main-menu.rkt

--- main-menu.rkt
+++ main-menu.rkt
@@ -2,60 +2,106 @@
 (require "game-window-class.rkt")
 (require "brick.rkt")
 (require "game-grid.rkt")
+(require "highscore.rkt")
 
-;;Canvas callback
+;;----------Canvas callbacks----------
+
 (define (render-start-menu canvas dc)
   (send dc set-font (make-font #:size 20))
-  (send dc draw-text "really. fun. game. right. here." 10 20))
+  (send dc draw-text "welcome to..." 160 20)
+  (send dc set-font (make-font #:size 28 #:underlined? #t))
+  (send dc draw-text "DON'T TOUCH THE LAVA!" 15 70 "black"))
 
-;;Button event functions
-(define (tutorial-func button event)
-  (void))
+(define (render-tutorial canvas dc)
+  (send dc set-font (make-font #:size 25 #:underlined? #t))
+  (send dc draw-text "Tutorial" 230 30))
 
-(define (roll-credits button event)
-  (void))
+(define (render-credits canvas dc)
+  (send dc set-font (make-font #:size 32 #:underlined? #t))
+  (send dc draw-text "Credits:" 215 30)
+  (send dc set-font (make-font #:size 20 #:underlined? #t))
+  (send dc draw-text "Developed and programmed by:" 10 120)
+  (send dc set-font (make-font #:size 18))
+  (send dc draw-text "- Christoffer Hejdström" 120 200)
+  (send dc draw-text "- Jonatan Gustafsson" 120 240)
+  (send dc set-font (make-font #:size 12))
+  (send dc draw-text "In a project in the course TDDC74, Linköping University spring 2015." 20 340))
 
-;;Canvas
+;;----------Canvases----------
+
 (define menu-canvas
   (new canvas%
        [parent menu-window]
        [paint-callback render-start-menu]))
 
-;;Buttons
-(define start-button
+(define tutorial-canvas
+  (new canvas%
+       [parent tutorial-window]
+       [paint-callback render-tutorial]))
+
+(define credits-canvas
+  (new canvas%
+       [parent credits-window]
+       [paint-callback render-credits]))
+
+;;----------Main Menu Buttons----------
+
+(define casual-button
   (new button%
        [parent menu-window]
        [label "casual..."]
        [callback (lambda (button event)
+                   (set-curr-win 'casual)
+                   (send curr-win init-score)
+                   (load-highscore 'classic)
                    (set-column-size 5)
                    (choose-bitmap 5)
                    (send menu-window show #f)
                    (send main-game-window show #t)
                    (send main-game-window refresh)
-                   (send main-game-window init-score)
                    (send start-window show #t))]))
 
-(define mode-button
+(define hyper-button
   (new button%
        [parent menu-window]
        [label "HYPER!!!"]
-       [callback (lambda (button event) 
+       [callback (lambda (button event)
+                   (set-curr-win 'hyper)
+                   (send curr-win init-score)
+                   (load-highscore 'hyper)
                    (set-column-size 3)
                    (choose-bitmap 3)
                    (send menu-window show #f)
                    (send hyper-window show #t)
                    (send hyper-window refresh)
-                   (send hyper-window init-score)
                    (send start-window show #t))]))
 
 (define tutorial-button
   (new button%
        [parent menu-window]
        [label "Tutorial!!!"]
-       [callback tutorial-func]))
+       [callback (lambda (button event)
+                   (send tutorial-window show #t))]))
 
 (define credits-button
   (new button%
        [parent menu-window]
        [label "Credits!!!"]
-       [callback roll-credits]))
+       [callback (lambda (button event)
+                   (send credits-window show #t))]))
+
+;;----------Close Buttons----------
+
+(define close-tutorial-button
+  (new button%
+       [parent tutorial-window]
+       [label "Close"]
+       [callback (lambda (button event)
+                   (send tutorial-window show #f))]))
+
+(define close-credits-button
+  (new button%
+       [parent credits-window]
+       [label "Close"]
+       [callback (lambda (button event)
+                   (send credits-window show #f))]))
