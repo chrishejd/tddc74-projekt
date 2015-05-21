4c9f02dc975252f139b81856560382c1c0718f52
jongu926 <jonatan_gsson@hotmail.com>
2015-05-21 23:50:46 +0200

    Update wrong-key.rkt

--- wrong-key.rkt
+++ wrong-key.rkt
@@ -1,8 +1,16 @@
 #lang racket/gui
+
+;;Purpose: Controls what happens when wrong key on the keyboard is pressed
+;;Authors: Christoffer Hejdström (chrhe465) and Jonatan Gustafsson (jongu926)
+;;Last change: Updated with new modular highscore and game modes, 2015-05-20
+
 (require "game-window-class.rkt")
 (require "highscore.rkt")
 (require "timer-start-window.rkt")
+
 (provide main-wrong-key)
+
+;;----------Canvas Drawing Function----------
 
 (define (render-wrong-key canvas dc)
   (let ((score (send curr-win get-score)))
@@ -11,16 +19,18 @@
     (send dc draw-text 
           (string-append (string-append "You stepped on " (number->string 
                                                            score)) 
-                         " blocks.")
-          90 100)))
+                         " blocks.") 90 100)))
 
-;;Canvas for failed game
+;;----------Canvas For Wrong Key----------
+
 (define wrong-key-canvas
   (new canvas%
        [parent wrong-key-window]
        [paint-callback render-wrong-key]))
 
-;;Main function for pressing the wrong key
+;;----------Main Function For Pressing The Wrong Key---------- 
+
+;;Saves score if it is good enough, stops timer etc.
 (define (main-wrong-key score)
   (send timer stop)
   (send main-game-window show #f)
@@ -30,6 +40,9 @@
   (send wrong-key-window show #t)
   (send wrong-key-window refresh))
 
+;;----------Buttons----------
+
+;;Restarts the game with the same mode
 (define retry-button
   (new button%
        [parent wrong-key-window]
@@ -41,7 +54,8 @@
                    (send curr-win refresh)
                    (send start-window show #t))]))
 
-(define exit-button
+;;Back to main menu
+(define main-menu-button
   (new button%
        [parent wrong-key-window]
        [label "Main menu"]
