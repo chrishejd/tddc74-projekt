c0a7a8e920ae7549016b8c41c778757ab6e757da
jongu926 <jonatan_gsson@hotmail.com>
2015-05-22 14:39:43 +0200

    Update main-menu.rkt

--- main-menu.rkt
+++ main-menu.rkt
@@ -15,11 +15,22 @@
   (send dc set-font (make-font #:size 20))
   (send dc draw-text "welcome to..." 160 20)
   (send dc set-font (make-font #:size 28 #:underlined? #t))
-  (send dc draw-text "DON'T TOUCH THE LAVA!" 15 70 "black"))
+  ;;(send dc set-text-foreground "red")
+  (send dc draw-text "DON'T TOUCH THE LAVA!" 15 70))
 
 (define (render-tutorial canvas dc)
-  (send dc set-font (make-font #:size 25 #:underlined? #t))
-  (send dc draw-text "Tutorial" 230 30))
+  (send dc set-font (make-font #:size 32 #:underlined? #t))
+  (send dc draw-text "Tutorial" 230 30)
+  (send dc set-font (make-font #:size 14))
+  (send dc draw-text "The game is to avoid the lava, and to do that you must" 20 100)
+  (send dc draw-text "jump on the black blocks floating in the lava." 20 130)
+  (send dc draw-text "The score system is based on how many blocks you manage" 20 160)
+  (send dc draw-text "to jump on in the time limit of 20 seconds. In order to" 20 190)
+  (send dc draw-text "jump to a block you must press the corresponding key on" 20 220)
+  (send dc draw-text "the keyboard of which the bottom block belongs to." 20 250)
+  (send dc draw-text "PS. You have time to prepare your route before the timer starts." 5 400)
+  (send dc set-font (make-font #:size 18))
+  (send dc draw-text "Remember: Don't touch the lava!" 80 325))
 
 (define (render-credits canvas dc)
   (send dc set-font (make-font #:size 32 #:underlined? #t))
@@ -86,14 +97,14 @@
 (define tutorial-button
   (new button%
        [parent menu-window]
-       [label "Tutorial!!!"]
+       [label "Tutorial"]
        [callback (lambda (button event)
                    (send tutorial-window show #t))]))
 
 (define credits-button
   (new button%
        [parent menu-window]
-       [label "Credits!!!"]
+       [label "Credits"]
        [callback (lambda (button event)
                    (send credits-window show #t))]))
 
